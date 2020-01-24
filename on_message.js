11f28fa0d6f8184cac23b13bd0b084df9699faba
notaprofi <jufrkide@mail.ru>
2020-01-24 16:26:52 +0300

    Added CountRoles and Help commands

--- on_message.js
+++ on_message.js
@@ -11,28 +11,77 @@ module.exports = {
            
             args = args.splice(1);
             switch(cmd) {
-                // !ping
-                case 'ping':
-                    this.ping(message);
+                case 'help':
+                    this.help(message);
                     break;
-                case 'list':
+                case 'notatest':
+                    this.test(message);
+                    break;
+                case 'listUsers':
                     this.listUsers(message);
                     break;
-             }
-         }
+                case 'countRoles':
+                    this.countRoles(message);
+                    break;
+            }
+        }
     },
 
-    ping: function(message) {
-        message.channel.send('pong');
+    help: function(message) {
+        var text = 'Available commands:\n';
+        text += '!help - show this text\n';
+        text += '!notatest - check if the bot is online\n';
+        text += '!listUsers - show users table\n';
+        text += '!countRoles - show roles table\n';
+        message.channel.send(text);
+    },
+
+    test: function(message) {
+        message.channel.send('Notabot is online');
     },
     
     listUsers: function(message) {
         var members = message.guild.members.array();
         var text = '';
-        text += members.length + ' users' + '\n';
+        text += members.length + ' members total.' + '\n';
         for(let member of members) {
             text += member.displayName + '\n';
         }
         message.channel.send(text);
-    }
+    },
+
+    countRoles: function(message) {
+        // create role array
+        var roles = message.guild.roles.array();
+        var roles_count = {};
+        for(let role of roles) {
+            roles_count[role.name] = 0;
+        }
+        // count roles
+        var members = message.guild.members.array();
+        for(let member of members) {
+            var member_roles = member.roles.array();
+            for(let mr of member_roles) {
+                roles_count[mr.name]++;
+            }
+        }
+        // output roles count
+        var text = '.\n';
+        for(let name in roles_count) {
+            text += this.pad(name) + '|\t\t' + roles_count[name] + '\n';
+        }
+        message.channel.send(text);
+    },
+
+    pad: function(str) {
+        var pad = Array(30).join(' ');
+        var padLeft = false;
+        if (typeof str === 'undefined') 
+          return pad;
+        if (padLeft) {
+          return (pad + str).slice(-pad.length);
+        } else {
+          return (str + pad).substring(0, pad.length);
+        }
+      }
 }
