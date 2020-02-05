ce1e269a94e8ec423d36277b80732d971c77a40e
notaprofi <jufrkide@mail.ru>
2020-02-05 16:39:14 +0300

    A function to check who were offline.

--- on_message.js
+++ on_message.js
@@ -9,7 +9,6 @@ module.exports = {
             var args = message.content.substring(1).split(' ');
             var cmd = args[0];
            
-            args = args.splice(1);
             switch(cmd) {
                 case 'help':
                     this.help(message);
@@ -23,6 +22,9 @@ module.exports = {
                 case 'countRoles':
                     this.countRoles(message);
                     break;
+                case 'inactivity':
+                    this.inactivity(message, args[1]);
+                    break;
             }
         }
     },
@@ -33,6 +35,7 @@ module.exports = {
         text += '!notatest - check if the bot is online\n';
         text += '!listUsers - show users table\n';
         text += '!countRoles - show roles table\n';
+        text += '!inactivity NDAYS - show people who are offline for NDAYS+ days\n';
         message.channel.send(text);
     },
 
@@ -80,7 +83,7 @@ module.exports = {
             if ( member_roles.length <= 1 ) { // only everyone
                 roles_count["{NO ROLE}"]++;
             }
-            
+
         }
         // output roles count
         var text = '```\n';
@@ -102,6 +105,88 @@ module.exports = {
         message.channel.send(text);
     },
 
+    inactivity: function(message, MaxOffDays = 180) {
+        const timePerUser = 5000;
+        var members = message.guild.members.array();
+        message.channel.send('I am looking for members with a role who are ' + MaxOffDays + ' or more days offline.\nI will be done in less than ' + members.length*timePerUser/60000 + ' minutes.');
+        var text = 'Here we go: \n';
+        var iLines = 0;
+        for(let iM = 0; iM < members.length; iM++) {
+            let member = members[iM];
+            if (member.hasPermission(['KICK_MEMBERS']) || member.roles.array().length == 1) {
+                member.nOffDays = 0; // don't check moderators and people with only @everyone role
+                continue;
+            }
+            setTimeout( function() {
+                console.log(member.displayName + " GO!\n");
+                module.exports.daysOffline(message, member, Number(MaxOffDays));
+            }, iM*timePerUser);
+        }
+        setTimeout( function() {
+            for(let member of members) {
+                if (member.nOffDays <= 0 || member.nOffDays == undefined) 
+                    continue;
+                text += '<@' + member.id + '>' + '\n';
+                iLines++;
+                if(iLines >= 1900/32) {
+                    message.channel.send(text);
+                    iLines = 0;
+                }
+            }
+            message.channel.send(text);
+            console.log("DONE!\n");
+        }, members.length*timePerUser);
+    },
+
+    /*daysOffline_next: function(message, member, N) {
+        console.log(member.displayName + " daysOffline_next\n");
+        return this.daysOffline_do(message, member, N).then(function(result) {
+            if (result === true) {
+                return this.daysOffline(message, member, N+30);
+            } else {
+                console.log(member.displayName + " N-30 = " + N-30);
+                return N-30;
+            }
+        });
+    },*/
+    
+    daysOffline: function(message, member, N) {
+        return new Promise(function(resolve, reject) {
+            var nGuests = 0;
+            var nGuestsPlus = 0;
+            message.guild.pruneMembers(N, true)
+            .then(function(result) {
+                nGuests = result;
+            }).then(() => {
+
+                var member_roles = member.roles.array();
+                var roles_backup = [];
+                for(let i = 0; i < member_roles.length; i++) {
+                    let role = member_roles[i];
+                    //console.log(role.name);
+                    roles_backup.push(role);
+                }
+
+                member.removeRoles(roles_backup)
+                .then(() => {
+
+                    nGuestsPlus = message.guild.pruneMembers(N, true).then(function(result) {
+                        nGuestsPlus = result;
+                    }).then(() => {
+                        member.addRoles(roles_backup).then(() => {
+                            console.log(" resolve = " + (nGuestsPlus > nGuests));
+                            if(nGuestsPlus > nGuests) {
+                                member.nOffDays = N;
+                            }
+                            resolve(nGuestsPlus > nGuests);
+                        });
+                    });
+                });
+            });
+
+        });
+    },
+
     pad: function(str, length = 30) {
         var pad = Array(length).join(' ');
         var padLeft = false;
@@ -112,5 +197,5 @@ module.exports = {
         } else {
           return (str + pad).substring(0, pad.length);
         }
-      }
+    }
 }
